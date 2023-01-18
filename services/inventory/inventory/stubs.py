5a904cb5154ce882d8b1555b57ba0faec0a5f006
filwaline <skywalker950925@gmail.com>
2023-01-18 21:41:36 +0800

    resolve_reference with external fields

--- services/inventory/inventory/stubs.py
+++ services/inventory/inventory/stubs.py
@@ -7,21 +7,7 @@ from strawberry.types import Info
 @strawberry.federation.type(keys=["upc"], shareable=True)
 class Product:
     upc: str
-    # #################################
-    # # resolve_reference don't work with @external together???
-
-    # inStock: bool
-
-    # @classmethod
-    # def resolve_reference(cls, info: Info, upc: str):
-    #     p = info.context.repo.find(upc)
-    #     if p:
-    #         return Product(
-    #             upc=p.upc,
-    #             inStock=p.inStock,
-    #         )
-
-    #################################
+    inStock: bool
 
     weight: int = strawberry.federation.field(external=True, default=0)
     price: int = strawberry.federation.field(external=True, default=0)
@@ -32,7 +18,13 @@ class Product:
             return 0
         return int(self.weight * 0.5)
 
-    @strawberry.field
-    def in_stock(self, info: Info) -> bool:
-        p = info.context.repo.find(self.upc)
-        return p.inStock if p else False
+    @classmethod
+    def resolve_reference(cls, info: Info, upc: str, **kwargs):
+        """
+        when using external, the external fields are passed to resolve reference
+
+        https://github.com/strawberry-graphql/strawberry/issues/2468#issuecomment-1387042614
+        """
+        p = info.context.repo.find(upc)
+        if p:
+            return Product(upc=p.upc, inStock=p.inStock, **kwargs)
